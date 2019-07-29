07e7c535dfb630631a83d956c141a76a04f4c3d6
SrFairyox <federiconieto04@gmail.com>
2019-07-29 19:02:32 -0300

    Update 2.0.0.11
    Bugfixes:
    --> Fixed max spin cost.

--- Corvus/Properties/AssemblyInfo.cs
+++ Corvus/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(true)]
 [assembly: Guid("4e3b77da-9856-4d1e-9bba-cb3260dc1f33")]
-[assembly: AssemblyVersion("2.0.0.10")]
-[assembly: AssemblyFileVersion("2.0.0.10")]
+[assembly: AssemblyVersion("2.0.0.11")]
+[assembly: AssemblyFileVersion("2.0.0.11")]
